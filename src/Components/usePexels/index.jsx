a584c5543f6b53c22cbda93e50920b4de8d4d96d
jdx <librededrogas420@gmail.com>
2023-07-19 08:28:38 -0500

    i remove comentarys innesesaries

--- src/Components/usePexels/index.jsx
+++ src/Components/usePexels/index.jsx
@@ -84,43 +84,7 @@ const usePexels = () => {
       }
       return []
   }
-    // useEffect(()=>{
-    //   if(query.length>2){  // console.log('this is in usepexels',pexels)
-    //   let dataFind =   pexels.find(item=>item.category === query) || [];
-    //   let dataSome =  pexels.some(item=>item.category === query);
-    //     if(!dataSome && pexelsReady){
-  
-    //     const Key = 'Q9H9FqFCzkajQ95I5ELBrO0CjrZlnFcRMFgsbLzJgb7sC5zRgXwwnJXp'
-      
-    //     const client =  createClient(Key) 
-    //     // let n=1
-        
-    //       try {
-            
-    //         client.photos.search({query,per_page:50})
-    //         .then(dt=>{
-    //           const photos = dt.photos.map(item=>{
-    //             return {'alt': item.alt, 'url':item.url, 'photographer':item.photographer, 'photographer_url':item.photographer_url, "src": item.src.original }
-    //           })
-    //           const dataPhotos = { photos: photos, category:query };
-          
-    //           // console.log(dt)
-    //           setData(dataPhotos)
-    //           handleSubmit(dt, query)
-    //           // console.log(n++)
-    //         })
-    //       } catch (error) {
-    //         console.log('error in use pexels',error)
-    //       }
-      
-          
-    //     }else{
-    //       setData( dataFind)
-    //     }
-    //  } 
-    
-    // },[pexels])
-  
+
 
   return {data, fetch}
 
